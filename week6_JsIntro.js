573ca1fcd94ee07bc2c26a9ba745319c30b4ee16
doublechou <30583873+doublechou@users.noreply.github.com>
2018-10-16 19:13:52 -0400

    week8

---
+++ week6_JsIntro.js
@@ -0,0 +1,13 @@
+var myVar = "name"
+var myArray = ["cat", "dog", "what"]
+
+console.log(myVar)
+console.log(myArray)
+document.write("<p>")
+if(true){
+  document.write("hello")
+
+}
+
+document.write("</p>")
+console.log("hello")
